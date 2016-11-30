a48c05f6a15c77f6f06b965dea702a527e0b1f7d
nilmagnum <leue@student.chalmers.se>
2016-11-30 07:29:55 +0100

    can query businesses from yelp now

---
+++ src/core/request.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const AUTH_TOKEN = 'lGw551u4p7Cex7Mev_iIjQJqvNLHZ548jfyBtABxNL_XpvPkqcUVKGmgRjcPZ2jeI5XmjaxAGAF9TUFfuLOBc4aC4stWmUVhs-EMXOQKkZV6ON15Uz1DqP0c4Wo-WHYx';
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + AUTH_TOKEN;
+
+export function requestYelp(location, limit, categories, callback) {
+	axios({
+		method: 'get',
+		url: createQueryString(location, limit, categories),
+	}).then(function (response) {
+		console.log(response.data);
+	})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
+
+function createQueryString(location, limit, categories) {
+	let baseURL = 'https://api.yelp.com/v3/businesses/search?';
+	baseURL += 'location=' + location;
+	baseURL += '&limit=' + limit;
+	baseURL += '&categories=' + categories;
+	return baseURL;
+}
+
+function yelpRequestAccessToken() {
+	axios({
+		method: 'post',
+		url: 'https://api.yelp.com/oauth2/token',
+		params: {
+			grant_type: 'client_credentials',
+			client_id: '-s4vos5gB61THchNR6Yn-Q',
+			client_secret: 'YGnDrqdSb5gobO0TsbbDlhnMJF89lVhuLx2AYqu8ayDRAwBFJV01InoQ4ZZj0NOP'
+		}
+	}).then(function (response) {
+		console.log(response);
+	})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
+
+requestYelp('magdeburg', 10, 'bars');
